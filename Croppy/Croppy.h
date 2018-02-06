96adafdcc3974c59c66a97f49ea38a63ff6a62de
khuong291 <dkhuong291@gmail.com>
2018-02-06 22:43:43 +0700

    Initial commit

---
+++ Croppy/Croppy.h
@@ -0,0 +1,19 @@
+//
+//  Croppy.h
+//  Croppy
+//
+//  Created by KhuongPham on 2/6/18.
+//  Copyright © 2018 KhuongPham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Croppy.
+FOUNDATION_EXPORT double CroppyVersionNumber;
+
+//! Project version string for Croppy.
+FOUNDATION_EXPORT const unsigned char CroppyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Croppy/PublicHeader.h>
+
+
