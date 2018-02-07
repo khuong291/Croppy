832bc7552868a1235b5c08f73f5fb04f9dd6bc32
khuong291 <dkhuong291@gmail.com>
2018-02-07 11:48:41 +0700

    Add rotate function

--- Croppy/UIImage+Croppy.swift
+++ Croppy/UIImage+Croppy.swift
@@ -57,9 +57,10 @@ extension UIImage {
             break
         }
         
-        // Now we draw the underlying CGImage into a new context, applying the transform
-        // calculated above.
-        let ctx = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: cgImage.bytesPerRow, space: cgImage.colorSpace!, bitmapInfo: cgImage.bitmapInfo.rawValue)!
+        // Now we draw the underlying CGImage into a new context, applying the transform calculated above.
+        guard let ctx = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: cgImage.bytesPerRow, space: cgImage.colorSpace!, bitmapInfo: cgImage.bitmapInfo.rawValue) else {
+            return self
+        }
         
         ctx.concatenate(transform)
         
@@ -68,19 +69,57 @@ extension UIImage {
             break
             
         case .rightMirrored:
-            draw(in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
+            ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
             break
             
         default:
-            draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
             break
         }
         
         // And now we just create a new UIImage from the drawing context.
-        let _cgImage = ctx.makeImage()!
+        guard let _cgImage = ctx.makeImage() else {
+            return self
+        }
         let _image = UIImage(cgImage: _cgImage)
         
         return _image
+    }
+    
+    func rotate(byAngle angleInRadians: CGFloat) -> UIImage {
+        guard let cgImage = cgImage else {
+            return self
+        }
+        // Calculate the size of the rotated image.
+        let rotatedImageFrame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        let rotatedImageSize = rotatedImageFrame.applying(CGAffineTransform(rotationAngle: angleInRadians)).size
+        
+        // Create a bitmap-based graphics context.
+        UIGraphicsBeginImageContextWithOptions(rotatedImageSize, false, scale)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return self
+        }
+        
+        // Move the origin of the user coordinate system in the context to the middle.
+        context.translateBy(x: rotatedImageSize.width / 2, y: rotatedImageSize.height / 2)
+        
+        // Rotates the user coordinate system in the context.
+        context.rotate(by: angleInRadians)
+        
+        // Flip the handedness of the user coordinate system in the context.
+        context.scaleBy(x: 1, y: -1)
+        
+        // Draw the image into the context.
+        context.draw(cgImage, in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
+        
+        guard let rotatedImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            return self
+        }
+        
+        UIGraphicsEndImageContext()
+        
+        return rotatedImage
     }
 }
 
