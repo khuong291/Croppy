4e43e7b312f902b976699eef7ddbb514b04e565b
khuong291 <dkhuong291@gmail.com>
2018-02-13 19:37:06 +0700

    Update CGGeometry+Croppy

--- Croppy/CGGeometry+Croppy.swift
+++ Croppy/CGGeometry+Croppy.swift
@@ -28,3 +28,20 @@ func croppyRectScaleAroundPoint(rect: CGRect, point: CGPoint, sx: CGFloat, sy: C
 func croppyPointIsNull(point: CGPoint) -> Bool {
     return point.equalTo(croppyPointNull)
 }
+
+func croppyPointRotateAroundPoint(point: CGPoint, pivot: CGPoint, angle: CGFloat) -> CGPoint {
+    var point = point
+    var translationTransform = CGAffineTransform(translationX: -pivot.x, y: -pivot.y)
+    point = point.applying(translationTransform)
+    let rotationTransform = CGAffineTransform(rotationAngle: angle)
+    point = point.applying(rotationTransform)
+    translationTransform = CGAffineTransform(translationX: pivot.x, y: pivot.y)
+    point = point.applying(translationTransform)
+    return point
+}
+
+func croppyPointDistance(p1: CGPoint, p2: CGPoint) -> CGFloat {
+    let dx = p1.x - p2.x
+    let dy = p1.y - p2.y
+    return sqrt(pow(dx, 2) + pow(dy, 2))
+}
